966832f94eac5fd3af912b87f1c20ef31109700c
Neelam Chavan <neelamchavan410@gmail.com>
2022-04-09 14:34:15 +0530

    search and render list of data on form submit

--- src/App.js
+++ src/App.js
@@ -6,22 +6,33 @@ function App() {
   const APP_KEY = 'e4a0f84cec5c4138cbda21a6c01fae5c'
 
   const [recipes, setRecipes] = useState([])
+  const [search, setSearch] = useState("")
+  const [query, setQuery] = useState('chicken')
 
   useEffect(() => {
     getRecipes()
-  }, [])
+  }, [query])
 
   const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`)
     const data = await response.json()
     console.log(data);
     setRecipes(data.hits)
   }
 
+  const updateSearch = e => {
+    setSearch(e.target.value)
+  }
+
+  const submitHandler = e => {
+    e.preventDefault()
+    setQuery(search)
+  }
+
   return (
     <div className="app">
-      <form className="search-form">
-        <input type="text" className="search-bar" />
+      <form className="search-form" onSubmit={submitHandler}>
+        <input type="text" className="search-bar" value={search} onChange={updateSearch} />
         <button type="submit" className="search-btn">Search</button>
       </form>
       {
