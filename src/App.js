a4608b4d1c0d132ffed690b709c04896226043be
Neelam Chavan <neelamchavan410@gmail.com>
2022-05-03 18:10:27 +0530

    update search on input change

--- src/App.js
+++ src/App.js
@@ -9,17 +9,11 @@ function App() {
   const APP_KEY = 'e4a0f84cec5c4138cbda21a6c01fae5c'
 
   const [recipes, setRecipes] = useState([])
-  const [defaultRecipes, setDefaultRecipes] = useState([])
   const [search, setSearch] = useState("")
   const [query, setQuery] = useState('banana')
-  const [cuisineType, setCuisineType] = useState("Indian")
-  const [isSubmitted, setIsSubmitted] = useState(false)
 
   useEffect(() => {
-    getDefaultRecipes()
-    if (isSubmitted) {
-      getRecipes()
-    }
+    getRecipes()
   }, [query])
 
   const getRecipes = async () => {
@@ -29,17 +23,9 @@ function App() {
     setRecipes(data.hits)
   }
 
-  const getDefaultRecipes = async () => {
-    setCuisineType("Indian")
-    const response = await fetch(`https://api.edamam.com/search?q=${query}&from=0&to=4&cuisineType=${cuisineType}&app_id=${APP_ID}&app_key=${APP_KEY}`)
-    const data = await response.json()
-    console.log(data);
-    setDefaultRecipes(data.hits)
-    setIsSubmitted(false)
-  }
-
   const updateSearch = e => {
     setSearch(e.target.value)
+    setQuery(e.target.value)
     getRecipes()
   }
 
@@ -47,7 +33,6 @@ function App() {
     e.preventDefault()
     setQuery(search)
     getRecipes()
-    // setIsSubmitted(false)
   }
 
   return (
@@ -56,16 +41,11 @@ function App() {
         <input type="text" className="search-bar" value={search} onChange={updateSearch} placeholder="Type any ingredient, recipe e.g. banana" />
         <button type="submit" className="search-btn">Search</button>
       </form>
-      <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <div style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }}>
         {
-          defaultRecipes.length == 0 ?
-            recipes.map(recipe => (
-              <Recipe key={recipe.recipe.label} title={recipe.recipe.label} calories={recipe.recipe.calories} img={recipe.recipe.image} ingredients={recipe.recipe.ingredients} />
-            ))
-            :
-            defaultRecipes.map(defaultRecipe => (
-              <DefaultRecipe key={defaultRecipe.recipe.label} title={defaultRecipe.recipe.label} img={defaultRecipe.recipe.image} />
-            ))
+          recipes.map(recipe => (
+            <Recipe key={recipe.recipe.label} title={recipe.recipe.label} calories={recipe.recipe.calories} img={recipe.recipe.image} ingredients={recipe.recipe.ingredients} />
+          ))
         }
       </div>
     </div>
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -3,15 +3,15 @@ import style from './Recipe.module.css'
 
 function Recipe(props) {
   return (
-    <div className={style.recipe}>
-      <h1>{props.title}</h1>
-      <ol>
+    <div className={style.container}>
+      {/* <ol>
         {props.ingredients.map((ingredient) => {
           return <li>{ingredient.text}</li>
         })}
-      </ol>
-      <div className={style.calories}>Calories:{props.calories}</div>
+      </ol> */}
+      {/* <div className={style.calories}>Calories:{props.calories}</div> */}
       <img className={style.img} src={props.img} alt="" />
+      <div className={style.title}>{props.title}</div>
     </div>
   )
 }
